a230855c0b6cf318b6d9b3d3f8ca9d1f02355b00
Luca G <luca.p.giannotti@gmail.com>
2024-06-24 02:30:12 -0500

    feat: altered color of sign-on button
    feat: added placeholder text to services.jsx to better represent the application being built

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -4,20 +4,20 @@ import './services.css';
 const Services = () => {
   return (
     <section className="services">
-      <h2>We're Scoda Agency</h2>
-      <p>The way we work is fun.</p>
+      <h2>This is Sun Beam</h2>
+      <p>Start your morning right with good news</p>
       <div className="service-cards">
         <div className="service-card">
-          <h3>Branding</h3>
-          <p>Details about branding service.</p>
+          <h3>Feature 1</h3>
+          <p>Details about the first feature.</p>
         </div>
         <div className="service-card">
-          <h3>Design & Development</h3>
-          <p>Details about design & development service.</p>
+          <h3>Feature 2</h3>
+          <p>Details about the second feature</p>
         </div>
         <div className="service-card">
-          <h3>Marketing</h3>
-          <p>Details about marketing service.</p>
+          <h3>Feature 3</h3>
+          <p>Details about the third feature</p>
         </div>
       </div>
     </section>
